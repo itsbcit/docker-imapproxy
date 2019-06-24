f6cd5f63ee7105739df5fa114535188ac5a195eb
Chris Wood <chriswood.ca@gmail.com>
2019-06-24 12:06:15 -0700

    fixed line length

--- 50-copy-imapproxy-config.sh
+++ 50-copy-imapproxy-config.sh
@@ -1,2 +1,3 @@
 TEMP_CONFFILE=/config/imapproxy.conf
-[ -f "$TEMP_CONFFILE" ] && { cp $TEMP_CONFFILE $IMAPPROXYD_CONF; echo "copied custom: $TEMP_CONFFILE"; } || echo "using default: $IMAPPROXYD_CONF"
+[ -f "$TEMP_CONFFILE" ] && { cp $TEMP_CONFFILE $IMAPPROXYD_CONF; echo "copied custom: $TEMP_CONFFILE"; } \
+	|| echo "using default: $IMAPPROXYD_CONF"
