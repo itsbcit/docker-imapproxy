c7123ea2d92a8495e3c5ee0c40fc110f53774e55
Chris Wood <chriswood.ca@gmail.com>
2019-06-24 12:04:21 -0700

    script for copying in a conf file from /config/

---
+++ 50-copy-imapproxy-config.sh
@@ -0,0 +1,2 @@
+TEMP_CONFFILE=/config/imapproxy.conf
+[ -f "$TEMP_CONFFILE" ] && { cp $TEMP_CONFFILE $IMAPPROXYD_CONF; echo "copied custom: $TEMP_CONFFILE"; } || echo "using default: $IMAPPROXYD_CONF"
