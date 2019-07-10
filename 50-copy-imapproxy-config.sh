b2261cb4679bff699f1b27f05569db8ae0ff1768
Juraj Ontkanin <Juraj_Ontkanin@bcit.ca>
2019-07-10 15:21:40 -0700

    cleanup

--- 50-copy-imapproxy-config.sh
+++ 50-copy-imapproxy-config.sh
@@ -1,6 +1,11 @@
-TEMP_CONFFILE="/config/imapproxy.conf"
-[ -f "$TEMP_CONFFILE" ] && { cp "$TEMP_CONFFILE" "$IMAPPROXYD_CONF"; echo "copied custom: $TEMP_CONFFILE"; } \
-	|| echo "using default: $IMAPPROXYD_CONF"
+TEMP_CONFFILE="$CONFIGDIR/imapproxy.conf"
+
+if [ -f "$TEMP_CONFFILE" ]; then
+    cp "$TEMP_CONFFILE" "$IMAPPROXYD_CONF"
+    echo "copied custom: $TEMP_CONFFILE"
+else
+    echo "using default: $IMAPPROXYD_CONF"
+fi
 
 if [ -f "$CONFIGDIR"/.DOCKERIZE.env ]; then
     echo "loading ${CONFIGDIR}/.DOCKERIZE.env environment"
